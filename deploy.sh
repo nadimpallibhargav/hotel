6a1c8f51f09f2122722b1d09b6962af03f459c42
bhargav27b <bhargav@sketchbrahma.com>
2022-08-06 12:19:20 +0530

    deleted unused files

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-aws s3 sync ./ s3://2hub-dev --region ap-south-1 --delete --exclude ".git/*" --exclude ".gitignore" --exclude ".gitlab-ci.yml" --exclude "README.md" --exclude "deploy.sh" --exclude "*.DS_Store"
